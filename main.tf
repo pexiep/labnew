9f80a650fcbb5835fd80278d252db86f6eca5557
hieuldt <46768555+hieuldt@users.noreply.github.com>
2021-07-05 15:55:20 +0700

    update

--- main.tf
+++ main.tf
@@ -16,33 +16,138 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-resource "aws_vpc" "quick_lab" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "Quick Lab"
+    Name = "Main"
   }
 }
 
-resource "aws_subnet" "quick_lab" {
-  vpc_id     = aws_vpc.quick_lab.id
+resource "aws_eip" "eib" {
+  vpc = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "IGW"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.eib.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "K8S Cluster"
+    Name = "Private"
   }
 }
 
-resource "aws_instance" "quick_lab" {
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
 
-  count = 3
-
-  ami           = "ami-0ab4d1e9cf9a1215a"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.quick_lab.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
   tags = {
-    Name = "Server ${count.index + 1}"
+    Name = "public_rt"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = {
+    Name = "private_rt"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
+
+resource "aws_route_table_association" "public_rta" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_rta" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
+
+}
+
+resource "aws_instance" "remote_gateway" {
+
+  ami                    = "ami-0ab4d1e9cf9a1215a"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name = "Remote Gateway"
+  }
+}
+
+resource "aws_instance" "protected_server" {
+  ami                    = "ami-0ab4d1e9cf9a1215a"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name = "Protected Server"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,29 @@
 output "vpc_cidr" {
   description = "VPC CIDR Block"
-  value       = aws_vpc.quick_lab.cidr_block
+  value       = aws_vpc.main.cidr_block
 }
 
 output "subnet_cidr" {
   description = "Subnet CIDR Block"
-  value       = aws_subnet.quick_lab.cidr_block
+  value       = aws_subnet.public.cidr_block
 }
 
-output "server_id" {
-  description = "Server name"
-  value       = ["${aws_instance.quick_lab.*.tags}"]
+# output "server_id" {
+#   description = "Server name"
+#   value       = ["${aws_instance.remote_gateway.*.tags}"]
+# }
+
+# output "server_ip" {
+#   description = "Server IP"
+#   value       = ["${aws_instance.remote_gateway.*.private_ip}"]
+# }
+
+output "igw_id" {
+  description = "IGW ID"
+  value       = aws_internet_gateway.igw.id
 }
 
-output "server_ip" {
-  description = "Server IP"
-  value       = ["${aws_instance.quick_lab.*.private_ip}"]
+output "rtb" {
+  description = "Route"
+  value       = aws_route_table.public_rt.route
 }
