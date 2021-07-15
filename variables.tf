0e39afd55b60b39149938d753f689b8c4de1ba1d
pexiep <74528809+pexiep@users.noreply.github.com>
2021-07-15 15:49:41 +0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,9 @@
 variable "access_key" {
      description = "Access key to AWS console"
-   
-}
-variable "secret_key" {
-     description = "Secret key to AWS console"
-     
 }
 
-variable "region" {
-  type        = string
-  description = "Region of the VPC"
+variable "secret_key" {
+     description = "Secret key to AWS console"
 }
 
 variable "key_name" {
@@ -18,19 +12,4 @@ variable "key_name" {
   sensitive   = true
 }
 
-variable "cidr_block" {
-  type        = string
-  description = "CIDR block for the VPC"
-}
 
-variable "public_subnet_cidr_blocks" {
-  default     = ["10.0.0.0/24", "10.0.2.0/24"]
-  type        = list
-  description = "List of public subnet CIDR blocks"
-}
-
-variable "availability_zones" {
-  default     = ["eu-west-3a", "eu-west-3b"]
-  type        = list
-  description = "List of availability zones"
-}
