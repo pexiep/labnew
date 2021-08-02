158297ce504a4f202d220aeb2cdfa722d5f34d6d
pexiep <74528809+pexiep@users.noreply.github.com>
2021-08-02 21:55:54 +0700

    Update and rename main.tf to nonprod.tf

---
+++ nonprod.tf
@@ -0,0 +1,163 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+#1 - Create VPC
+
+resource "aws_vpc" "nonprod" {
+  cidr_block       = "10.1.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "nonprodVPC"
+  }
+}
+
+#2- Create Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.nonprod.id
+
+  tags = {
+    Name = "gw"
+  }
+}
+
+#3 - Create Route Table
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.nonprod.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+	
+  tags = {
+    Name = "PublicRT"
+  }
+}
+
+#4 - Create 9 subnets at 3 seperate AZs (enable auto assign IPv4)
+
+resource "aws_subnet" "publicsubnet1" {
+  vpc_id     = aws_vpc.nonprod.id
+  cidr_block = "10.1.0.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet1"
+  }
+}
+
+resource "aws_subnet" "privatesubnet1" {
+  vpc_id     = aws_vpc.nonprod.id
+  cidr_block = "10.1.10.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PrivateSubnet1"
+  }
+}
+
+resource "aws_subnet" "dbsubnet1" {
+  vpc_id     = aws_vpc.nonprod.id
+  cidr_block = "10.1.100.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "DBSubnet1"
+  }
+}
+
+resource "aws_subnet" "publicsubnet2" {
+  vpc_id     = aws_vpc.nonprod.id
+  cidr_block = "10.1.1.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet2"
+  }
+}
+
+resource "aws_subnet" "privatesubnet2" {
+  vpc_id     = aws_vpc.nonprod.id
+  cidr_block = "10.1.11.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "privatesubnet2"
+  }
+}
+
+resource "aws_subnet" "dbsubnet2" {
+  vpc_id     = aws_vpc.nonprod.id
+  cidr_block = "10.1.110.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "dbsubnet2"
+  }
+}
+
+resource "aws_subnet" "publicsubnet3" {
+  vpc_id     = aws_vpc.nonprod.id
+  cidr_block = "10.1.2.0/24"
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet3"
+  }
+}
+
+resource "aws_subnet" "privatesubnet3" {
+  vpc_id     = aws_vpc.nonprod.id
+  cidr_block = "10.1.22.0/24"
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "privatesubnet3"
+  }
+}
+
+resource "aws_subnet" "dbsubnet3" {
+  vpc_id     = aws_vpc.nonprod.id
+  cidr_block = "10.1.220.0/24"
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "dbsubnet3"
+  }
+}
+
+#5 Create NAT Gateway
+
+resource "aws_nat_gateway" "example" {
+  subnet_id     = "10.1.1.0/24"
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+
+
