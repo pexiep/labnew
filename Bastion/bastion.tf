c8490dfdb43f0dff2b892e941f9b26cf81bc5b36
hiep <hple@gft.com>
2021-08-04 20:34:24 +0700

    Enter

--- Bastion/bastion.tf
+++ Bastion/bastion.tf
@@ -130,3 +130,68 @@ resource "aws_nat_gateway" "ngw2" {
   allocation_id = aws_eip.nat2.id 
   subnet_id = aws_subnet.publicbastion3.id 
 } 
+
+#7 - Create Security Groups
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow web traffic"
+  vpc_id      = aws_vpc.main.id
+
+	ingress {
+         from_port = 22
+         to_port = 22
+         protocol = "tcp"
+         cidr_blocks = ["0.0.0.0/0"]
+    }
+  egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+#7.1 Create Network interface NIC and EIP security group allow port ssh 22, and nat gateway with ingress and outgress based on that one
+
+resource "aws_network_interface" "bastion1-nic" {
+  subnet_id       = aws_subnet.bastion.id
+  private_ips     = ["10.3.10.5"]
+  security_groups = [aws_security_group.allow_web.id]
+}
+
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.bastion1-nic.id
+  associate_with_private_ip = "10.3.10.5"
+}
+
+#8 - Create EC2 for 3 bastion host
+
+resource "aws_instance" "web-server-instance" {
+    ami = "ami-09e67e426f25ce0d7"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+    key_name = var.key_name
+
+    network_interface {
+      device_index = 0
+      network_interface_id = aws_network_interface.web-server-nic.id
+    }
+
+    user_data = <<-EOF
+                #!bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo Hello from THE ANH > /var/www/html/index.html'
+                EOF
+    tags = {
+   	 Name = "webserver"
+    }
+}
