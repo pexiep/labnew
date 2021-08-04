8c4e97f703781ab4bb27c3203b11a31930e736ed
pexiep <74528809+pexiep@users.noreply.github.com>
2021-08-04 20:53:09 +0700

    Update eks.tf

--- nonprod/eks.tf
+++ nonprod/eks.tf
@@ -32,7 +32,7 @@ resource "aws_eks_cluster" "aws_eks" {
   role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids = "10.1.11.0/24"
+    subnet_ids = aws_subnet.privatesubnet2.id
   }
 
   tags = {
