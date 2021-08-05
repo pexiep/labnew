533090735384bb940a891702653375929178d9b9
pexiep <74528809+pexiep@users.noreply.github.com>
2021-08-05 11:03:39 +0700

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -32,7 +32,7 @@ resource "aws_eks_cluster" "aws_eks" {
   role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids =  ["subnet-030085d838b8fdfa8", "subnet-0a987c052868f834b", "subnet-0a4ae9a9cf2aedc75"]
+    subnet_ids =  [aws_subnet.privatesubnet1.id, aws_subnet.privatesubnet2.id, aws_subnet.privatesubnet3.id]
   }
 
   tags = {
@@ -78,7 +78,7 @@ resource "aws_eks_node_group" "node" {
   cluster_name    = aws_eks_cluster.aws_eks.name
   node_group_name = "node_test"
   node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = ["subnet-030085d838b8fdfa8", "subnet-0a987c052868f834b", "subnet-0a4ae9a9cf2aedc75"]
+  subnet_ids      = [aws_subnet.privatesubnet1.id, aws_subnet.privatesubnet2.id, aws_subnet.privatesubnet3.id]
 
   scaling_config {
     desired_size = 3
